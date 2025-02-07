3aeee02f9d2407bcbcb83951f3278e13aaef91f7
Aayush Nagpal <aayushnagpal90@gmail.com>
2025-02-08 02:11:12 +0530

    Added playlist feature

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { client } from "@/sanity/lib/client";
 import React, { Suspense } from "react";
-import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
+import { STARTUP_BY_ID_QUERY , PLAYLIST_BY_SLUG_QUERY } from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
 import { formatDate } from "@/lib/utils";
 import Link from "next/link";
@@ -8,6 +8,9 @@ import Image from "next/image";
 import markdownit from "markdown-it";
 import { Skeleton } from "@/components/ui/skeleton";
 import View from "@/components/View";
+import { StartupTypeCard } from "@/components/StartupCard";
+import StartupCard from "@/components/StartupCard"
+
 
 const md = markdownit();
 
@@ -16,6 +19,10 @@ export const experimental_ppr = true;
 const page = async ({ params }: { params: { id: string } }) => {
   const { id } = params;  // No need to await params
   const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+  const {select : editorPosts} = await client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+    slug : "editor-picks"
+  })
   
   if (!post) return notFound();
   
@@ -59,7 +66,17 @@ const page = async ({ params }: { params: { id: string } }) => {
         </div>
         <hr className="divider !w-full" />
 
-        {/* TODO: Editor for current startup */}
+        {editorPosts.length > 0 && (
+          <div className="max-w-4xl mx-auto">
+              <p className="text-30-semibold">Editor Picks</p>
+            <ul className="mt-7 card_grid-sm">
+              {editorPosts.map((post: StartupTypeCard , index : number) => (
+                <StartupCard key={index} post={post} />
+                
+              ))}
+            </ul>
+             </div>
+        )}
 
         <Suspense fallback={<Skeleton className="view_skeleton"/>}>
           <View id={id} />
